7d8ecc15a09d6d3d4b4e7fe62c4223f860d62182
HardAndHeavy <HardAndHeavyMetal@yandex.ru>
2025-09-23 05:49:12 +0300

    add lint for ror

--- ror/app/app/controllers/qr_controller.rb
+++ ror/app/app/controllers/qr_controller.rb
@@ -1,25 +1,24 @@
 
 class QrController < ApplicationController
   def generate
-    data = params[:data] || 'Default data'
-    
+    data = params[:data] || "Default data"
+
     qr = RQRCode::QRCode.new(data)
-    
+
     png = qr.as_png(
       bit_depth: 1,
       border_modules: 0,
       color_mode: ChunkyPNG::COLOR_GRAYSCALE,
-      color: 'black',
+      color: "black",
       file: nil,
-      fill: 'white',
+      fill: "white",
       module_px_size: 6,
       size: 120,
       uppercase: true
     )
-    
+
     respond_to do |format|
-      format.png { send_data png.to_s, type: 'image/png', disposition: 'inline' }
+      format.png { send_data png.to_s, type: "image/png", disposition: "inline" }
     end
   end
 end
-
--- ror/app/config/routes.rb
+++ ror/app/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  root to: ->(env) { [200, {}, ["This is the RoR framework in DevBox."]] }
+  root to: ->(env) { [ 200, {}, [ "This is the RoR framework in DevBox." ] ] }
 
-  get '/qr', to: 'qr#generate', defaults: { format: :png }
+  get "/qr", to: "qr#generate", defaults: { format: :png }
 end
